fb6a4db8e9fa57517bf104a61c4a7045eb57b901
yogitataluja <65607076+yogitataluja@users.noreply.github.com>
2021-03-07 17:45:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $('button').click(function(){
+        const name= $('#nameid').val()
+        alert(name)
+        $('#imgchange').attr('src',`https://joeschmoe.io/api/v1/${name}
+`)
+    })
+})
